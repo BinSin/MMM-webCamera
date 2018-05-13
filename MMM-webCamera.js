cab3b0d9f7db0f4fe3266b8efc8c69b3c2c2d389
BinSin <binyong318@gmail.com>
2018-05-13 22:03:20 +0900

    20180512

--- MMM-webCamera.js
+++ MMM-webCamera.js
@@ -12,14 +12,22 @@ Module.register("MMM-webCamera", {
 	},
 
   start: function() {
-		Log.log("Start webCamera");
+	  Log.log("Start webCamera");
+	  var self = this;
+	  this.sendSocketNotification("CAMERA_INIT", self.config);
   },
 
   notificationReceived: function(notification, payload, sender) {
 	  if(sender) {
 		if (notification === "COMMAND") {
-			if (payload === " take a picture"){
+			if (payload === " camera on"){
 				this.sendSocketNotification("CAMERA_ON", payload);
+			}
+			else if (payload === " take a picture") {
+				this.sendSockectNotification("TAKE_A_PICTURE", payload);
+			}
+			else if (payload === " camera off") {
+				this.sendSockectNotification("CAMERA_OFF", payload);
 			}
 		}
 	  }
--- node_helper.js
+++ node_helper.js
@@ -4,17 +4,59 @@
 'use strict';
 
 var NodeHelper = require("node_helper");
-//var NodeWebcam = require("node-webcam");
-//var Webcam = NodeWebcam.create({});
+var WebCamera = require("webcam.js");
+
+	var wcm = new WebCamera({
+		videoTag: document.getElementyId("video"),
+		constraints: {
+			video: {
+				width: 640,
+				height: 480,
+			}
+		}
+	});
 
 module.exports = NodeHelper.create({
+
+  initCamera: function(payload) {
+	var self = this;
+
+  }
 	
   socketNotificationReceived: function(notification, payload) {
     if (notification === 'CAMERA_ON') {
-  //    NodeWebcam.capture( "my_picture", {}, function( err, data ) {
- //       if ( !err ) console.log( "Image created!" );
-   //   });
+
+	    wcm.startCamera();
+	    wcm.grabFrame().then(function(imageBitmap) {
+	    var canvas = document.getElementById("canvas");
+	    canvas.width = imageBitmap.width;
+	    canvas.height = imageBitmap.height;
+	    var ctx = canvas.getContext("2d");
+	    ctx.drawImage(imageBitmap, 0, 0);
+	    });
     }
-  }
+    else if (notification === "TAKE_A_PICTURE") {
+	wcm.takePhoto().then(function(blob) {
+		return window.createImageBitmap(blob);
+	}).then(function (imageBitmap) {
+		var canvas = document.getElementById("canvas");
+		canvas.width = imageBitmap.width;
+		canvas.height = imageBitmap.height;
+		var ctx = canvas.getContext("2d");
+		ctx.drawImage(imageBitmap, 0, 0);
+	});
+    }
+    else if (notification === "CAMERA_OFF") {
+	 //   wcm.stopCamera();
+
+    }
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+	  if(notification == "CAMERA_INIT") {
+		  console.log("camera initializing");
+		  this.initCamera(payload);
+	  }
+  },
 
 });
