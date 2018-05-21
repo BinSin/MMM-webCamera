f0dd8c0bb7cc1532b25648b40e499891cb65e585
BinSin <binyong318@gmail.com>
2018-05-21 12:47:05 +0900

    defined

--- MMM-webCamera.js
+++ MMM-webCamera.js
@@ -1,25 +1,36 @@
+/*
+ * Author : BinSin
+ * https://github.com/BinSin/MMM-webCamera
+ */
 
 Module.register("MMM-webCamera", {
-	defaults: {
-		opts: [
-			{
-    	  			width: 1280,
-				height: 720,
-				quality: 100,
-				delay: 0,
-				saveShots: true,
-				output: "jpg",
-				device: false,
-				callbackReturn: "location",
-      				verbose: false
-			},
-		],
-	},
+  defaults: {
+	opts: [
+		{
+    	  		width: 1280,
+			height: 720,
+			quality: 100,
+			delay: 0,
+			saveShots: true,
+			device: false,
+			output: "jpg",
+			callbackReturn: "location",
+      			verbose: false,
+		},
+	],
+  },
 
   start: function() {
 	 var self = this;
 	 Log.log("Starting module: " + this.name);
-		this.sendSocketNotification("INIT_CAMERA", self.config);
+
+  },
+
+  getDom: function() {
+	 var wrapper = document.createElement("div");
+	 wrapper.innerHTML = "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS55KmFrEQHUiHC1akm_tApSeDRBEb5a4X_h6udq5t8HWqRz9ym' width='300' height='240' />";
+	 wrapper.className = "picture";
+	 return wrapper;
   },
 
   notificationReceived: function(notification, payload, sender) {
@@ -28,10 +39,18 @@ Module.register("MMM-webCamera", {
 		if (notification == "COMMAND") {
 			if (payload == " take a picture") {
 				console.log("take a picture");
-				self.sendSocketNotification("TAKE_A_PICTURE", payload);
+				self.sendSocketNotification("TAKE_A_PICTURE", self.config.opts[0].output);
 			}
 		}
 	 }
   },
+  
+   socketNotificationReceived: function(notification, payload) {
+	   var self = this;
+	   if(notification == "SEND_SOCKET") {
+		   console.log("send path to AWS");
+		   self.sendNotification("SEND_AWS", payload);
+	   }
+   },
 
 });
--- node_helper.js
+++ node_helper.js
@@ -7,8 +7,6 @@ var NodeHelper = require("node_helper");
 var NodeWebcam = require("node-webcam");
 var exec = require('child-process-promise').exec;
 var moment = require('moment');
-var opts = {};
-var Webcam = null;
 
 module.exports = NodeHelper.create({
 
@@ -18,40 +16,24 @@ module.exports = NodeHelper.create({
 
   initCamera: function(payload) {
     var self = this;
-    this.Webcam = NodeWebcam.create( {
-      width: payload.opts.width,
-      height: payload.opts.height,
-      quality: payload.opts.quality,
-      delay: payload.opts.delay,
-      saveShots: payload.opts.saveShots,
-      output: payload.opts.output,
-      device: payload.opts.device,
-      callbackReturn: payload.opts.callbackReturn,
-      verbose: payload.opts.verbose
-    } );
+    this.Webcam = NodeWebcam.create(payload.opts);
   },
 
   socketNotificationReceived: function(notification, payload) {
-    if(notification == "INIT_CAMERA") {
+    if (notification == "INIT_CAMERA") {
       this.initCamera(payload);
     }
     else if (notification == "TAKE_A_PICTURE") {
       var self = this;
       
-      var picture_path = "~/Pictures/" + moment().format('YYMMDD_HHmmss');
+      var filename = moment().format('YYMMDD_HHmmss');
+      var picture_path = "~/Pictures/" + filename;
       self.Webcam.capture( picture_path, function( err, data ) {} );
+      self.path = picture_path + "." + payload;
       
-      setTimeout(function() {
-        exec('sudo fbi -T 2 ' + picture_path + '.' + 'jpg');
-      }, 4500);
-        
-      setTimeout(function() {
-        exec("sudo /bin/kill $(ps aux | grep '$search_terms' | grep -v 'grep' | awk '{print $2}')");
-      }, 7500);
-
-      self.sendNotification("AWS_MESSAGE", payload);
+      self.sendSocketNotification("SEND_SOCKET", picture_path);
     }
 
-  },
+   },
 
 });
